ecc68db580da0bcf5b7c4140fc5a78d86ff4fd93
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-11-16 11:15:17 +0300

    add sql2o places dao class template

---
+++ src/main/java/dao/Sql2oPlacesDao.java
@@ -0,0 +1,38 @@
+package dao;
+
+import models.Places;
+import models.Reviews;
+
+import java.util.List;
+
+public class Sql2oPlacesDao implements PlacesDao{
+    @Override
+    public void addPlace(Places place) {
+
+    }
+
+    @Override
+    public List<Places> getAllPlaces() {
+        return null;
+    }
+
+    @Override
+    public Places getPlaceById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Reviews> getReviewsByPlace(int placeId) {
+        return null;
+    }
+
+    @Override
+    public void deleteAllPlaces() {
+
+    }
+
+    @Override
+    public void deletePlaceById(int id) {
+
+    }
+}
