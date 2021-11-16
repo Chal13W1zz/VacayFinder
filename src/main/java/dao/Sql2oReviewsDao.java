292ead941f0eea2b5d3dc2dd6293a6f64c2a0f3b
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-11-16 11:22:14 +0300

    add sql2o reviews dao class template

---
+++ src/main/java/dao/Sql2oReviewsDao.java
@@ -0,0 +1,32 @@
+package dao;
+
+import models.Reviews;
+
+import java.util.List;
+
+public class Sql2oReviewsDao implements ReviewsDao{
+    @Override
+    public void addReview(Reviews review) {
+
+    }
+
+    @Override
+    public List<Reviews> getAllReviews() {
+        return null;
+    }
+
+    @Override
+    public Reviews getReviewById(int id) {
+        return null;
+    }
+
+    @Override
+    public void deleteAllReviews() {
+
+    }
+
+    @Override
+    public void deleteReviewById(int id) {
+
+    }
+}
