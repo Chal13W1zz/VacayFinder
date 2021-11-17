487f853876a0695f68fabb36b4f14fdd6bfc390e
BRIAN MAKAU <brian.mutiso@student.moringaschool.com>
2021-11-17 05:54:39 +0300

    Add route for the form page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -21,6 +21,9 @@ public class App {
             return new ModelAndView(model, "place-form.hbs");
         }, new HandlebarsTemplateEngine() );
 
-
+        get("/places/new",(request, response)->{
+            Map<String, Object>model = new HashMap<>();
+            return new ModelAndView(model, "review-form.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
